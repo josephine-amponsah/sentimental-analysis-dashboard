f206623c1350784be51426366c241ea6dfe6c034
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2022-08-18 17:20:44 +0000

    app layout - date picker styling

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__)
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 
 adsData = pd.read_csv(r'C:\\Users\\HP\\Downloads\\mybooks\\project\\ads-proj\\ad-sales-ml-proj\\data\\marketingData.csv',  index_col= 0)
 revData = pd.read_csv(r'C:\\Users\\HP\\Downloads\\mybooks\\project\\ads-proj\\ad-sales-ml-proj\\data\\revenueData.csv',  index_col= 0)
@@ -39,18 +39,45 @@ app.layout = html.Div([
         dbc.Col(
                 dbc.Row([
         dbc.Row([
-            dbc.Col(html.Div("MSG"), width = 3),
-            dbc.Col(html.Div("SEARCH"), width = 3),
-            dbc.Col(html.Div("NOTIFS & PROFILE"), width = 4),
+            dbc.Col(html.Div(
+                children=[html.Div("Hi Marketing Manager", style={'fontWeight': 'bold'}), html.Div("Welcome back!")]), width = 3),
+            dbc.Col(
+                html.Div([
+                    html.I(className = "bi bi-search"),
+                    html.I(className ="bi bi-bell-fill"),
+                    html.I(className ="bi bi-person-circle"),
+                    ],className = 'icon-bar'), width = 3), 
+            ], justify = 'between'),
+        html.Br(),
+        dbc.Row(style = {'height':'20px'}),
+        dbc.Row([
+            dbc.Col(html.Div([
+                dcc.DatePickerRange( id = "dash-date-filter",  
+                                    month_format='MMM Do, YY',
+                                    end_date_placeholder_text='MMM Do, YY',
+                                    start_date= "03 01 19",
+                                    calendar_orientation = 'horizontal',
+                                    day_size= 30), 
+                html.Div(id = "dash-date-filter-range") ], 
+                             className = 'dashboard-date-filter'
+                ),width = 5 ),
+            dbc.Col(
+                dbc.Button(
+                    "DOWNLOAD",
+                    className = 'download-button'
+                ), width = 2
+            )
+            ], justify = 'between', className = 'filter-bar'),
+        html.Br(),
+        dbc.Row([
+            html.Div("Graphs")
             ]),
-        dbc.Row([]),
                 ]),
                 className = 'dashboard'
                 , width=10)
     ]),
-    
-
 ])
+
 app.callback()
 
 if __name__ == '__main__':
