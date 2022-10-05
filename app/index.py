51969f58f1b065cb814fb5f76c6c127b885e09c5
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2022-10-05 20:54:26 +0000

    renaming files and creating subfolders for pages

--- app/index.py
+++ app/index.py
@@ -1,70 +1,60 @@
 import dash
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
 import dash_bootstrap_components as dbc
-from dash import html, dcc
 from dash.dependencies import Input, Output
 
 
-# Connect to main app.py file
-import home
-from home import app
-from home import server
 
+# from app.pages import sales
 
-# Connect to your app pages
-from pages import sales, optimize, customerrev, campaign
+app = Dash(__name__, use_pages = True)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 
-
-# Navbar
-navbar = dbc.Col( 
+app.layout = html.Div([
+    dbc.Row([
+        dbc.Col([
+            dbc.Col( 
             html.Div([
             dbc.Nav("LOGO"),
             html.Br(),
             html.Div(
                      [
                          "ANALYTICS",
-                     dbc.Row([dbc.NavLink("Customer Review Analysis",href="")], align = 'left'),
-                     dbc.Row([dbc.NavLink("Customer Review Analysis",href="/pages/sales")]),
+                     dbc.Row([
+                         dbc.NavLink(page["name"],href= page["path" ]) 
+                              for page in dash.page_registry.values()
+                        if page["path"].startswith("app\pages\dashboard")
+                              ], align = 'left'),
+                     
                      ], className =' nav-subtext'),  
             html.Hr(),
             html.Div(
                       [
                           "MENU",
-                     dbc.Row([dbc.NavLink("Plan Campaign",href="")]),
-                     dbc.Row([dbc.NavLink("Projections",href=" ")]),
-                     dbc.Row([dbc.NavLink("Customer",href="")]),
+                     dbc.Row([dbc.NavLink("Plan Campaign",href=[])]),
+                     dbc.Row([dbc.NavLink("Projections",href="http://")]),
+                     dbc.Row([dbc.NavLink("Customer",href="http://")]),
                      ], className =' nav-subtext'), 
             html.Hr(),
             dbc.Nav(["UTILITIES"], className ='nav-subtext')
             ], className = 'nav-card '),style={"height":"100%"}, 
             width = 2
-            ),
-
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    navbar,
-    html.Div(id='page-content', children=[])
+         ),
+        
+        dash.page_container
+    ])
 ])
+]
+)
 
 
-# Not making a 404 page, routing it directly to home
-default_template = app.layout
 
 
-@home.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/apps/sales':
-        return sales.layout
-    elif pathname == '/apps/campaign':
-        return campaign.layout
-    elif pathname == '/apps/optimize':
-        return optimize.layout
-    elif pathname == '/apps/customerrev':
-        return customerrev.layout
-    else:
-        return default_template
+
+
 
 
 if __name__ == '__main__':
-    home.run_server(debug=True)
+    app.run_server(debug=True)
