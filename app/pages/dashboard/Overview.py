f9dcf5914854306675070cfa8bf39565a933ff87
Josephine Amponsah Baah <97001821+ladyjossy77@users.noreply.github.com>
2022-11-07 01:02:56 +0000

    fixed over flowing layout of "overview page"

--- app/pages/dashboard/Overview.py
+++ app/pages/dashboard/Overview.py
@@ -8,7 +8,7 @@ from dash.dependencies import Input, Output
 
 # from app.pages import sales
 
-dash.register_page(__name__, path="/dashboard/overview")
+dash.register_page(__name__, path="/dashboard/")
 #app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP])
 # server = app.server
 
@@ -20,63 +20,61 @@ cardNames = ["Titles", "Titles", "Titles", "Titles"]
 
 layout = html.Div([
     dbc.Row([
-        dbc.Col(
-            dbc.Row([
-                    dbc.Row([
-                        dbc.Col(html.Div(
-                            children=[html.Div("Hi Marketing Manager", style={'fontWeight': 'bold'}), html.Div("Welcome back!")]), width=3),
-                        dbc.Col(
-                            html.Div([
-                                html.I(className="bi bi-search"),
-                                html.I(className="bi bi-bell-fill"),
-                                html.I(className="bi bi-person-circle"),
-                            ], className='icon-bar'), width=3),
-                    ], justify='between'),
-                    html.Br(),
-                    dbc.Row(style={'height': '20px'}),
-                    dbc.Row([
-                        dbc.Col(html.Div([
-                            dcc.DatePickerRange(id="dash-date-filter",
-                                                month_format='MMM Do, YY',
-                                                end_date_placeholder_text='MMM Do, YY',
-                                                start_date="03 01 19",
-                                                calendar_orientation='horizontal',
-                                                day_size=25,
-                                                style={'fontSize': '10px', }
-                                                ),
-                            html.Div(id="dash-date-filter-range")],
-                            className='dashboard-date-filter'
-                        ), width=5),
-                        dbc.Col(
-                            dbc.Button(
-                                "DOWNLOAD",
-                                className='download-button'
-                            ), width=2
-                        )
-                    ], justify='between', className='filter-bar'),
-                    html.Br(),
-                    dbc.Row(style={'height': '20px'}),
-                    html.Div([
-                        html.Div([
-                            html.Div("Analysis Overview"),
-                            html.Br(),
-                            dbc.Row([
-                                dbc.Col(dbc.Card(
-                                    children=[html.Div(title), html.Div("value", className='value'), html.Div("description")], className='summary-cards',),
-                                    width=3,)
-                                for title in cardNames
-                            ], justify="evenly")
-                        ], className='summary-section'),
-                        dbc.Row(style={'height': '20px'}),
-                        html.Div([
-                            html.Div("graph title"),
-                            html.Br(),
-                            dcc.Graph("graph")
-                        ], className='summary-section')
-                    ]),
-                    ]),
-            className='dashboard')
-    ]),
+        dbc.Row([
+            dbc.Col(html.Div(
+                children=[html.Div("Hi Marketing Manager", style={'fontWeight': 'bold'}), html.Div("Welcome back!")]), width=3),
+            dbc.Col(
+                html.Div([
+                    html.I(className="bi bi-search"),
+                    html.I(className="bi bi-bell-fill"),
+                    html.I(className="bi bi-person-circle"),
+                ], className='icon-bar'), width=3),
+        ], justify='between'),
+        html.Br(),
+        dbc.Row(style={'height': '20px'}),
+        dbc.Row([
+            dbc.Col(html.Div([
+                dcc.DatePickerRange(id="dash-date-filter",
+                                    month_format='MMM Do, YY',
+                                    end_date_placeholder_text='MMM Do, YY',
+                                    start_date="03 01 19",
+                                    calendar_orientation='horizontal',
+                                    day_size=25,
+                                    style={'fontSize': '10px', }
+                                    ),
+                html.Div(id="dash-date-filter-range")],
+                className='dashboard-date-filter'
+            ), width=4),
+            dbc.Col([
+                dbc.Button(
+                    "DOWNLOAD",
+                    className='download-button'
+                )], width=2
+            )
+        ], justify='between', className='filter-bar'),
+        html.Br(),
+        dbc.Row(style={'height': '20px'}),
+        html.Div([
+            html.Div([
+                html.Div("Analysis Overview"),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col(dbc.Card(
+                        children=[html.Div(title), html.Div("value", className='value'), html.Div("description")], className='summary-cards',),
+                        width=3,)
+                    for title in cardNames
+                ], justify="evenly")
+            ], className='summary-section'),
+            dbc.Row(style={'height': '20px'}),
+            html.Div([
+                html.Div("graph title"),
+                html.Br(),
+                dcc.Graph("graph")
+            ], className='summary-section')
+        ]),
+    ],),
+
+
 
 ])
 
